2d9563d8cd0b17e87de6ab33150219947f93c242
Shizhao Chen <shichen@redhat.com>
2021-12-06 00:13:29 +0800

    Intermediate config

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -2,14 +2,50 @@
 ;;; Commentary:
 ;;; Code:
 
+;; My mumble config
 (setq-default fill-column 80)
+
 (defun my-c++-mode-hook ()
   (setq c-basic-offset 8)
   (c-set-offset 'substatement-open 0))
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
+
 (setq-default c-basic-offset 8)
 
 (global-set-key (kbd "M-o") 'switch-window)
+
+(setq org-adapt-indentation nil)
+
+;; From "Linux kernel coding style"
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              ;; Enable kernel mode for the appropriate files
+              (when (and filename
+                         (string-match (expand-file-name "~/github/linux")
+                                       filename))
+                (setq indent-tabs-mode t)
+                (setq show-trailing-whitespace t)
+                (c-set-style "linux-tabs-only")))))
 
 (provide 'init-preload-local)
 ;;; init-preload-local.el ends here
