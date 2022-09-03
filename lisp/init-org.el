0a0a1e29d4018ff722a056c4e011730e414aa114
Shizhao Chen <shichen@redhat.com>
2022-09-04 01:51:26 +0800

    using org-roam and some other minor updates

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -163,7 +163,8 @@ typical word processor."
 (setq org-todo-keywords
       (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
               (sequence "PROJECT(p)" "|" "DONE(d!/!)" "CANCELLED(c@/!)")
-              (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")))
+              (sequence "WAITING(w@/!)" "DELEGATED(e!)" "HOLD(h)" "|" "CANCELLED(c@/!)")
+              (sequence "NEW(n)" "POST(p)" "TESTED(e)" "MODIFIED(m)" "VERIFIED(v)")))
       org-todo-repeat-to-state "NEXT")
 
 (setq org-todo-keyword-faces
--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -6,7 +6,14 @@
 ;;; Code:
 
 ;; Enable orgmode template shortcuts.
-;; (require 'org-tempo)
+(require 'org-tempo)
+
+;; (global-set-key (kbd "C-c C-e") 'eval-region)
+
+;; (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+;;                          ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
+;;                          ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu/")
+;;                          ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
 
 ;; Globalized column width.
 (setq-default fill-column 80)
@@ -25,6 +32,8 @@
 ;; (pyvenv-mode 1)
 
 ;; Use kernel style guide in c-mode.
+(defvar c-syntactic-element)
+(defvar c-basic-offset)
 (defun c-lineup-arglist-tabs-only (ignored)
   "IGNORED Line up argument lists by tabs, not spaces."
   (let* ((anchor (c-langelem-pos c-syntactic-element))
@@ -55,6 +64,46 @@
                 (setq show-trailing-whitespace t)
                 (c-set-style "linux-tabs-only")))))
 
+
+;; Org-roam configurations
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(require 'use-package)
+
+;; (load-theme 'leuven t)
+
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "/home/shichen/notes/roam"))
+  (setq org-roam-dailies-directory "daily/")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more
+  ;; informative completion interface
+  (setq org-roam-node-display-template
+        (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+
+(setq org-journal-dir "/home/shichen/notes/roam/journal")
+
+
+;; (setq org-agenda-files (list "~/notes/roam"))
+
+(global-set-key (kbd "C-x f") 'select-frame-by-name)
 
 (provide 'init-preload-local)
 ;;; init-preload-local.el ends here
