23942a49112a00f88875a1f37f8890ce1ca42949
Jean Rolland <jr@dhcp-93-160.nay.redhat.com>
2022-10-08 14:21:27 +0800

    [roam] use customized org-roam-node-display-template

--- lisp/init-roam.el
+++ lisp/init-roam.el
@@ -3,6 +3,17 @@
 
 ;;; Code:
 
+;; The following definition is required by customizing display template
+(cl-defmethod org-roam-node-type ((node org-roam-node))
+  "Return the TYPE of NODE."
+  (condition-case nil
+      (file-name-nondirectory
+       (directory-file-name
+        (file-name-directory
+         (file-relative-name (org-roam-node-file node) org-roam-directory))))
+    (error "")))
+
+
 (use-package org-roam
   :ensure t
   :custom
@@ -19,7 +30,8 @@
   ;; If you're using a vertical completion framework, you might want a more
   ;; informative completion interface
   (setq org-roam-node-display-template
-        (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+        (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
